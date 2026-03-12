1a4ba8eecfa0512e32b9e2ba82671d936e43bdca
sniemczyk423 <sniemczyk423@gmail.com>
2026-03-12 21:32:09 +0100

    Create-Interface

--- ProjectManagerApp/src/main/java/com/example/projectmanagerapp/ProjectManagerAppApplication.java
+++ ProjectManagerApp/src/main/java/com/example/projectmanagerapp/ProjectManagerAppApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EntityScan("org.example.projectmanagerapp.entity")
-@EnableJpaRepositories("org.example.projectmanagerapp.repository")
+@EntityScan("com.example.projectmanagerapp.entity")
+@EnableJpaRepositories("com.example.projectmanagerapp.repository")
 public class ProjectManagerAppApplication {
 
     public static void main(String[] args) {
