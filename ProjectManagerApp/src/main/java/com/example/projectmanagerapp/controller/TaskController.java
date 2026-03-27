c03ab24e8c10de57c053149424019806a60c6d7d
sniemczyk423 <sniemczyk423@gmail.com>
2026-03-27 21:34:09 +0100

    Add User/Task API

---
+++ ProjectManagerApp/src/main/java/com/example/projectmanagerapp/controller/TaskController.java
@@ -0,0 +1,28 @@
+package com.example.projectmanagerapp.controller;
+
+import com.example.projectmanagerapp.entity.Task;
+import com.example.projectmanagerapp.repository.TaskRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    private final TaskRepository taskRepository;
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+}
