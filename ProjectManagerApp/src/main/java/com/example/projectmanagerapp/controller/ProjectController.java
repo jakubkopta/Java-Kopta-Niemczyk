530af996b1e9fd192b22a40cd10fd65821df70da
Jakub Kopta <jakubkopta@gmail.com>
2026-03-27 21:17:49 +0100

    Project Controller

---
+++ ProjectManagerApp/src/main/java/com/example/projectmanagerapp/controller/ProjectController.java
@@ -0,0 +1,29 @@
+package com.example.projectmanagerapp.controller;
+
+import com.example.projectmanagerapp.entity.Project;
+import com.example.projectmanagerapp.repository.ProjectRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+
+    private final ProjectRepository projectRepository;
+
+    public ProjectController(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @GetMapping
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    @PostMapping
+    public Project createProject(@RequestBody Project project) {
+        project.setId(null);
+        return projectRepository.save(project);
+    }
+}
